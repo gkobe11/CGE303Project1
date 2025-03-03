21812770ae2d37d812594ccd550b30ef0b4a06e1
gkobe11 <gavinkobe0@gmail.com>
2025-03-03 15:13:14 -0600

    added object interaction

---
+++ CGE303Project1/Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    private bool isPlayerNearby = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isPlayerNearby = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isPlayerNearby = false;
+    }
+
+    private void Interact()
+    {
+        // initiate foraging mini game here
+        Debug.Log("Interacted with object");
+    }
+}
