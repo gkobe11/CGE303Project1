dcdaff5dea23a9a12c4aa48d884cf3f22e8beb2a
mimi015 <mdavi5086@365.elmhurst.edu>
2025-03-09 12:18:42 -0500

    inventory and dragged item

---
+++ My project/Assets/Scripts/DinoInteraction.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoInteraction : MonoBehaviour
+{
+    private bool isPlayerNearby = false;
+    private bool isTamed = false;
+
+    public GameObject dinoTextBox; // set in inspector
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isPlayerNearby = true;
+        dinoTextBox.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isPlayerNearby = false;
+        dinoTextBox.SetActive(false);
+    }
+
+    private void Interact()
+    {
+        // initiate foraging mini game here
+        Debug.Log("Interacted with object"); //can be deleted
+
+
+
+
+        // if player successfully tames dino
+        isTamed = true;
+        
+    }
+}
