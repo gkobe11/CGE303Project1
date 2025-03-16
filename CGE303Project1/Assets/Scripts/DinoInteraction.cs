3350d5ebeff073fc6111a03e6e699f33fdfcfef6
gkobe11 <gavinkobe0@gmail.com>
2025-03-16 01:30:29 -0500

    fixed taming game

--- CGE303Project1/Assets/Scripts/DinoInteraction.cs
+++ CGE303Project1/Assets/Scripts/DinoInteraction.cs
@@ -1,46 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class DinoInteraction : MonoBehaviour
 {
     private bool isPlayerNearby = false;
-    private bool isTamed = false;
+    public bool isTamed = false;
+    public bool isTaming = false;
 
+    public GameObject dino; // set in inspector
     public GameObject dinoTextBox; // set in inspector
+    public GameObject tamingGame; // set in inspector
+
+    public TMP_Text textBox; // set in inspector
 
 
     // Update is called once per frame
     void Update()
     {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E) && !isTamed & !isTaming)
+            if (isPlayerNearby && Input.GetKeyDown(KeyCode.E) && !isTamed & !isTaming) //also must check if player has enough resources
+            {
+                Interact();
+            }
+
+        if (isTamed)
         {
-            Interact();
+            dino.GetComponent<BoxCollider2D>().enabled = false; //allow player to walk through dino
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         isPlayerNearby = true;
-        dinoTextBox.SetActive(true);
+        if (!isTamed)
+        {
+            dinoTextBox.SetActive(true);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         isPlayerNearby = false;
-        dinoTextBox.SetActive(false);
+        if (!isTamed)
+        {
+            dinoTextBox.SetActive(false);
+        }
     }
 
     private void Interact()
     {
-        // initiate foraging mini game here
-        Debug.Log("Interacted with object"); //can be deleted
+        dinoTextBox.SetActive(false);
+        isTaming = true;
+        //check if player has enough items to start taming
+        tamingGame.SetActive(true); //start taming mini game
+        StartCoroutine(wait());
+    }
 
-
-
-
-        // if player successfully tames dino
-        isTamed = true;
-        
+    IEnumerator wait()
+    {
+        while (tamingGame.activeSelf)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(5);
+        textBox.text = "";
     }
 }
