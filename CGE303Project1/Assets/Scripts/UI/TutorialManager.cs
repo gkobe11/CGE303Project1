d1cf611719ea31c00e6e35efc4e1c24238f813a2
gkobe11 <gavinkobe0@gmail.com>
2025-03-20 14:41:21 -0500

    fixed stretched text and inventory covering dialog panel

--- CGE303Project1/Assets/Scripts/UI/TutorialManager.cs
+++ CGE303Project1/Assets/Scripts/UI/TutorialManager.cs
@@ -5,27 +5,61 @@ using TMPro;
 
 public class TutorialManager : MonoBehaviour
 {
-    public GameObject foragingPanel;
-    public GameObject tutorialPanel;
+    public GameObject foragingPanel; // set in inspector
+    public GameObject tutorialPanel; // set in inspector
+
+    public GameObject inventoryCanvas; // set in inspector
+    public GameObject UICanvas; // set in inspector
+    public GameObject StartPanel; // set in inspector
 
     public bool firstForage = false;
     public bool firstTame = false;
     private bool forageTutorialActivated = false;
     private bool tameTutorialActivated = false;
 
+
+    private void Start()
+    {
+        inventoryCanvas.SetActive(false);
+        UICanvas.SetActive(false);
+
+        StartCoroutine(wait(StartPanel));
+    }
+
     void Update()
     {
         if (firstForage && !forageTutorialActivated)
         {
+            inventoryCanvas.SetActive(false);
+            UICanvas.SetActive(false);
+
             foragingPanel.SetActive(true);
             forageTutorialActivated = true;
+
+            StartCoroutine(wait(foragingPanel));
         }
 
         if (firstTame && !tameTutorialActivated)
         {
+            inventoryCanvas.SetActive(false);
+            UICanvas.SetActive(false);
+
             tutorialPanel.SetActive(true);
             tameTutorialActivated = true;
+
+            StartCoroutine(wait(tutorialPanel));
         }
 
+    }
+
+    IEnumerator wait(GameObject panel)
+    {
+        while (panel.activeSelf)
+        {
+            yield return null;
+        }
+
+        inventoryCanvas.SetActive(true);
+        UICanvas.SetActive(true);
     }
 }
